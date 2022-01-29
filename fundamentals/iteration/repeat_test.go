5fe97b47b5b9278a808793e55e637738e4f01a2f
lucasnevespereira <pereiraneveslucas@gmail.com>
2022-01-29 01:50:12 +0100

    added iteration

---
+++ fundamentals/iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a")
+	expected := "aaaaa"
+
+	if got != expected {
+		t.Errorf("got %q but expected %q", got, expected)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b")
+	fmt.Println(repeated)
+	// Output: bbbbb
+}
